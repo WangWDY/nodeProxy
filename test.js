a2623b7ee14fa0169c854eea2b9953830e2e40d5
WangWDY <2609457135@qq.com>
2018-12-25 16:51:41 +0800

    last

--- test.js
+++ test.js
@@ -8,3 +8,15 @@ git status  //查看当前状态(是否有修改文件)
 
 
 
+git add -A  //提交所有修改文件到暂存区
+
+
+git commit - m "zheShiCeShi"  //提交暂存区文件，起名为 zheShiCeShi  ，到本地仓库
+
+
+git log--pretty = oneline  //一行显示修改日志，显示起的名和 4096c30a1703e0ed0da05edc8078fffd6d37423f 代码
+
+
+git reset--hard afb5   // 最后面加那个黄色的代码前几个字符，可以跳到相应版本。
+
+git push origin master // 把当前的推送到远程仓库
